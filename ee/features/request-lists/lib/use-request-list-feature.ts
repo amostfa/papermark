6b3072e8112a6bf92e7d7f93f79bfef88d0ca30a
Ahmed <mr.a.alashwal@gmail.com>
2026-08-13 13:23:40 +0200

    Create use-request-list-feature.ts

---
+++ ee/features/request-lists/lib/use-request-list-feature.ts
@@ -0,0 +1,6 @@
+// Self-host stub: this Enterprise Edition module is not published in the
+// open-source Papermark repository. The request-lists feature is disabled.
+/* eslint-disable */
+// @ts-nocheck
+export const useRequestListFeatureEnabled: any = () => false;
+export default undefined as any;
