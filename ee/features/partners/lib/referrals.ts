5689f49f8b773c8f3e6db131abcc5112f852cd7c
Ahmed <mr.a.alashwal@gmail.com>
2026-08-13 13:22:36 +0200

    Create referrals.ts

---
+++ ee/features/partners/lib/referrals.ts
@@ -0,0 +1,6 @@
+// Self-host stub: this Enterprise Edition module is not published in the
+// open-source Papermark repository. The referrals feature is disabled.
+/* eslint-disable */
+// @ts-nocheck
+export const isReferralsEnabled: any = () => false;
+export default undefined as any;
